6af341309b960f1404e5d106e4b62661873f768b
Mike Tigas <mike@tig.as>
2010-12-15 12:50:13 -0800

    newcache fix

--- server/cacheutil/__init__.py
+++ server/cacheutil/__init__.py
@@ -1,55 +1,75 @@
+from django.db.models import Model as DjangoModel
 from django.core.cache import cache
-from hashlib import sha512
-from django.utils.encoding import smart_str
 
-USING_MEMCACHED = (cache.__module__ == 'django.core.cache.backends.memcached')
-USING_DUMMY_CACHE = (cache.__module__ == 'django.core.cache.backends.dummy')
+try:
+    from newcache import CacheClass as newcache_CacheClass
+    using_newcache = issubclass(cache.__class__, newcache_CacheClass)
+except:
+    using_newcache = False
 
+from hashlib import md5
+
+# Make this compatible with django-newcache and Django 1.3 (which has support for key hashing)
+from django.conf import settings
+FLAVOR = getattr(settings, 'FLAVOR', '')
+CACHE_VERSION = str(getattr(settings, 'CACHE_VERSION', 1))
+
+# Faster than default newcache.get_key since it only coerces to string (utf-8) if it's given a unicode
+# and uses the significantly CPU-faster md5
+def get_key(cachename):
+    # 3.6 to 4.4 usec/pass
+    if type(cachename) == unicode:
+        key = cachename.encode('utf-8')
+    else:
+        key = cachename
+    
+    hashed = md5(key).hexdigest()
+    return ''.join((FLAVOR, '-', str(CACHE_VERSION), '-', hashed))
+
+# compatibility with older code we have
 def _get_real_cachename(cachename):
-    """
-    Memcached is picky about key names.  They have to be <= 250 characters and cannot contain whitespace
-    or control characters.  This function creates "safe" keys by replacing those control characters and
-    applying a SHA256 hash if the key is >250 chars long.
+    from warnings import warn
+    warn('_get_real_cachename() is now get_key()',DeprecationWarning, stacklevel=2)
     
-    Benefits:
-        * Super long keys don't need to be truncated.  The hash function keeps them unique while shortening.
-        * Cache contents cannot be introspected: this means cache data can't simply be browsed by key,
-          which provides a little privacy protection if we are caching user-specific data.
-    """
-    # Only need this on memcached
-    if not USING_MEMCACHED:
+    if using_newcache:
         return cachename
-    
-    cachename = cachename.replace('\n','\\n').replace('\r','\\r').replace('\t','\\t').replace(' ','\\_')
-    cachename = smart_str(cachename,errors="backslashreplace")
-    if len(cachename) > 250:
-        # Concatenate part of the original string to avoid collisions and not waste the 250char limit
-        cachename = "%s%s" % (cachename[:122], sha512(cachename).hexdigest())
-    return cachename
+    else:
+        return get_key(cachename)
 
 def safe_get_cache(cachename):
     """ Gets an item from the cache (converting the given string into an always valid cache key) """
-    return cache.get( _get_real_cachename(cachename) )
-
-def safe_set_cache(cachename,obj,cachetime=None):
-    """ Puts an item into the cache (converting the given string into an always valid cache key) """
-    if cachetime is None:
-        # Use the default cachetime from settings
-        cache.set(
-            _get_real_cachename(cachename),
-            obj
-        )
+    
+    # django-newcache will perform _get_real_cachename internally
+    if using_newcache:
+        key = cachename
     else:
-        cache.set(
-            _get_real_cachename(cachename),
-            obj,
-            cachetime
-        )
+        key = get_key(cachename)
+    
+    return cache.get(key)
+
+def safe_set_cache(cachename, obj, timeout=None):
+    """ Puts an item into the cache (converting the given string into an always valid cache key) """
+    
+    if using_newcache:
+        key = cachename
+    else:
+        key = get_key(cachename)
+    
+    cache.set(
+        key,
+        obj,
+        timeout
+    )
     return obj
 
 def safe_del_cache(cachename):
     """ Deletes an item from the cache (converting the given string into an always valid cache key) """
-    cache.delete( _get_real_cachename(cachename) )
+    if using_newcache:
+        key = cachename
+    else:
+        key = get_key(cachename)
+    
+    cache.delete(key)
 
 # The following are based on work from http://fi.am/entry/low-level-cache-decorators-for-django/
 def cached_method(func, cachetime=None):
@@ -58,32 +78,51 @@ def cached_method(func, cachetime=None):
         key = 'cached_method_%s_%s_%s' % \
             (func.__name__, hash(args), hash(frozenset(kwargs.items())))
         val = safe_get_cache(key)
-        if val is None:
-            return safe_set_cache(key, func(*args, **kwargs), cachetime)
-        else:
-            return val
+        return safe_set_cache(key, func(*args, **kwargs), cachetime) if val is None else val
     return cached_func
 
+# INCORRECTLY NAMED: this is actually an "instance method" decorator
 def cached_clsmethod(func, cachetime=None):
-    """ Decorator for class methods """
+    """ Decorator for instance methods """
     def cached_func(self, *args, **kwargs):
-        key = 'cached_clsmethod_%s_%s_%s_%s_%s' % \
-            (self.__class__.__name__, func.__name__, self.pk, hash(args), hash(frozenset(kwargs.items())))
-        val = safe_get_cache(key)
-        if val is None:
-            return safe_set_cache(key, func(self, *args, **kwargs), cachetime)
+        if issubclass(self.__class__, DjangoModel):
+            clsname = "%s.%s" % (self.__class__._meta.app_label, self.__class__.__name__)
         else:
-            return val
+            clsname = self.__class__.__name__
+        
+        key = 'cached_clsmethod_%s_%s_%s_%s_%s' % \
+            (clsname, func.__name__, getattr(self,'pk',hash(self)), hash(args), hash(frozenset(kwargs.items())))
+        
+        val = safe_get_cache(key)
+        return safe_set_cache(key, func(self, *args, **kwargs), cachetime) if val is None else val
     return cached_func
+
+def cached_klsmethod(func, cachetime=None):
+    """ Decorator for class methods """
+    def cached_func(cls, *args, **kwargs):
+        if issubclass(cls, DjangoModel):
+            clsname = "%s.%s" % (cls._meta.app_label, cls.__name__)
+        else:
+            clsname = cls.__name__
+        
+        key = 'cached_clsmethod_%s_%s_%s_%s_%s' % \
+            (clsname, func.__name__, hash(cls), hash(args), hash(frozenset(kwargs.items())))
+        
+        val = safe_get_cache(key)
+        return safe_set_cache(key, func(cls, *args, **kwargs), cachetime) if val is None else val
+    return classmethod(cached_func)
 
 def cached_property(func, cachetime=None):
     """ Decorator for class properties """
     def cached_func(self):
-        key = 'cached_property_%s_%s_%s' % \
-            (self.__class__.__name__, func.__name__, self.pk)
-        val = safe_get_cache(key)
-        if val is None:
-            return safe_set_cache(key, func(self), cachetime)
+        if issubclass(self.__class__, DjangoModel):
+            clsname = "%s.%s" % (self.__class__._meta.app_label, self.__class__.__name__)
         else:
-            return val
+            clsname = self.__class__.__name__
+        
+        key = 'cached_property_%s_%s_%s' % \
+            (clsname, func.__name__, getattr(self,'pk',hash(self)))
+        
+        val = safe_get_cache(key)
+        return safe_set_cache(key, func(self), cachetime) if val is None else val
     return property(cached_func)
